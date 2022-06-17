f61a29485cbb2d07dd07d6263b9d22b111380181
daniela reyes <danielareyessicacha@gmail.com>
2022-06-17 07:41:56 -0500

    seguimiento17-06-2022

--- database/migrations/2022_05_13_114300_create_cargos_table.php
+++ database/migrations/2022_05_13_114300_create_cargos_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('cargos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 50);
+            $table->timestamps();
+
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('cargos');
     }
 };
